3d0b0efe86860c758a680401a3402ba548823f96
kmskillings <kmskillings@gmail.com>
2024-09-22 20:15:20 -0600

    Create spi_master.vhd basic entity

---
+++ accelerometer_test/spi_master.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity spi_master is
+  port (
+    clock_master : in std_logic;
+    clock_spi : out std_logic
+  ) ;
+end spi_master ;
+
+architecture rtl of spi_master is
+
+
+
+begin
+
+
+
+end architecture ; -- rtl
