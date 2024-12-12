2fd07c06f490b72963ae10cd5495216dc64feb3b
kmskillings <kmskillings@gmail.com>
2024-12-11 21:24:32 -0700

    Tweak names.

--- vhdl/vga_common.vhd
+++ vhdl/vga_common.vhd
@@ -11,7 +11,7 @@ library ieee ;
 
 package vga_common is
 
-  constant c_vga_phase_name_length : natural := 16;
+  constant c_phase_name_length : natural := 16;
 
   type t_phase_id is (
     SYNC,
@@ -22,29 +22,29 @@ package vga_common is
 
   subtype t_phase_duration is positive;
 
-  type t_vga_phase is record
+  type t_phase is record
     id : t_phase_id;
     duration : t_phase_duration;
-    name : string(1 to c_vga_phase_name_length);
+    name : string(1 to c_phase_name_length);
   end record;
 
-  type t_vga_phase_sequence is array (natural range <>) of t_vga_phase;
+  type t_sequence is array (natural range <>) of t_phase;
 
   -- Returns the duration of the longest phase in a sequence.
   function get_longest_duration(
-    sequence : t_vga_phase_sequence
+    sequence : t_sequence
   ) return t_phase_duration;
   
 end package ;
 
 package body vga_common is
 
-  function get_longest_phase(
-    sequence : t_vga_phase_sequence
+  function get_longest_duration(
+    sequence : t_sequence
   ) return t_phase_duration is
     
-    variable longest_phase : t_vga_phase := sequence(0);
-    variable phase : t_vga_phase;
+    variable longest_phase : t_phase := sequence(0);
+    variable phase : t_phase;
 
   begin
 
@@ -57,6 +57,6 @@ package body vga_common is
     
     return longest_phase.duration;
 
-  end get_longest_phase;
+  end get_longest_duration;
 
 end vga_common;
